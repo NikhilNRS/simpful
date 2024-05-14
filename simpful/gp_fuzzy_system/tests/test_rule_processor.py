298b41807a2ca095e9292425bb4f5660836a02b1
Nikhil Razab-Sekh <nikhil_rs@live.com>
2024-05-14 23:33:04 +0200

    chore: improve formatting of rules even more

--- simpful/gp_fuzzy_system/tests/test_rule_processor.py
+++ simpful/gp_fuzzy_system/tests/test_rule_processor.py
@@ -1,50 +1,47 @@
 import unittest
 import sys
 sys.path.append('..')
-from rule_processor import strip_parentheses, find_clauses, reintroduce_parentheses, handle_not_conditions, format_rule
+from rule_processor import *
 
 
-# class TestRuleFormatting(unittest.TestCase):
-#     def test_format_rules(self):
-#         rules = [
-#             "IF ((gdp_growth_annual_prcnt IS Low) AND (unemployment_rate_value IS High)) THEN (PricePrediction IS PricePrediction)",
-#             "IF ((trade_balance_value IS Low) OR (foreign_direct_investment_value IS Low)) THEN (PricePrediction IS PricePrediction)",
-#             "IF ((spy_close IS High) OR (volume IS Low)) THEN (PricePrediction IS PricePrediction)",
-#             "IF ((gld_close IS Low) AND (macd IS Negative)) THEN (PricePrediction IS PricePrediction)",
-#             "IF ((gld_close IS High) AND (foreign_direct_investment_value IS High)) THEN (PricePrediction IS PricePrediction)",
-#             "IF ((spy_close IS Low) AND (volume IS High)) THEN (PricePrediction IS PricePrediction)",
-#             "IF (inflation_rate_value IS Medium) THEN (PricePrediction IS PricePrediction)",
-#             "IF ((gdp_growth_annual_prcnt IS High) OR ( NOT (unemployment_rate_value IS Low))) THEN (PricePrediction IS PricePrediction)",
-#             "IF ((macd IS Positive) OR (rsi IS Oversold)) THEN (PricePrediction IS PricePrediction)",
-#             "IF ((volume IS High) AND (spy_close IS High)) THEN (PricePrediction IS PricePrediction)",
-#             "IF ((volume IS High) AND (spy_close IS High)) THEN (PricePrediction IS PricePrediction)",
-#             "IF ((PaO2 IS low) AND ((Trombocytes IS high) AND ((Creatinine IS high) AND (BaseExcess IS normal)))) THEN (Sepsis IS low_probability)",
-#             "IF ((PaO2 IS high) AND ((Trombocytes IS low) AND ((Creatinine IS low) AND ( NOT (BaseExcess IS normal))))) THEN (Sepsis IS high_probability)"
-#         ]
+class TestRuleFormatting(unittest.TestCase):
+    def test_rule_formatting(self):
+        rules = [
+            "IF ((gdp_growth_annual_prcnt IS Low) AND (unemployment_rate_value IS High)) THEN (PricePrediction IS PricePrediction)",
+            "IF ((trade_balance_value IS Low) OR (foreign_direct_investment_value IS Low)) THEN (PricePrediction IS PricePrediction)",
+            "IF ((spy_close IS High) OR (volume IS Low)) THEN (PricePrediction IS PricePrediction)",
+            "IF ((gld_close IS Low) AND (macd IS Negative)) THEN (PricePrediction IS PricePrediction)",
+            "IF ((gld_close IS High) AND (foreign_direct_investment_value IS High)) THEN (PricePrediction IS PricePrediction)",
+            "IF ((spy_close IS Low) AND (volume IS High)) THEN (PricePrediction IS PricePrediction)",
+            "IF (inflation_rate_value IS Medium) THEN (PricePrediction IS PricePrediction)",
+            "IF ((gdp_growth_annual_prcnt IS High) OR ( NOT (unemployment_rate_value IS Low))) THEN (PricePrediction IS PricePrediction)",
+            "IF ((macd IS Positive) OR (rsi IS Oversold)) THEN (PricePrediction IS PricePrediction)",
+            "IF ((volume IS High) AND (spy_close IS High)) THEN (PricePrediction IS PricePrediction)",
+            "IF ((PaO2 IS low) AND ((Trombocytes IS high) AND ((Creatinine IS high) AND (BaseExcess IS normal)))) THEN (Sepsis IS low_probability)",
+            "IF ((PaO2 IS high) AND ((Trombocytes IS low) AND ((Creatinine IS low) AND ( NOT (BaseExcess IS normal))))) THEN (Sepsis IS high_probability)"
+        ]
 
-#         expected_output = [
-#             "IF (gdp_growth_annual_prcnt IS Low) AND (unemployment_rate_value IS High) THEN (PricePrediction IS PricePrediction)",
-#             "IF (trade_balance_value IS Low) OR (foreign_direct_investment_value IS Low) THEN (PricePrediction IS PricePrediction)",
-#             "IF (spy_close IS High) OR (volume IS Low) THEN (PricePrediction IS PricePrediction)",
-#             "IF (gld_close IS Low) AND (macd IS Negative) THEN (PricePrediction IS PricePrediction)",
-#             "IF (gld_close IS High) AND (foreign_direct_investment_value IS High) THEN (PricePrediction IS PricePrediction)",
-#             "IF (spy_close IS Low) AND (volume IS High) THEN (PricePrediction IS PricePrediction)",
-#             "IF (inflation_rate_value IS Medium) THEN (PricePrediction IS PricePrediction)",
-#             "IF (gdp_growth_annual_prcnt IS High) OR (NOT (unemployment_rate_value IS Low)) THEN (PricePrediction IS PricePrediction)",
-#             "IF (macd IS Positive) OR (rsi IS Oversold) THEN (PricePrediction IS PricePrediction)",
-#             "IF (volume IS High) AND (spy_close IS High) THEN (PricePrediction IS PricePrediction)",
-#             "IF (PaO2 IS low) AND (Trombocytes IS high) AND (Creatinine IS high) AND (BaseExcess IS normal) THEN (Sepsis IS low_probability)",
-#             "IF (PaO2 IS high) AND (Trombocytes IS low) AND (Creatinine IS low) AND (NOT(BaseExcess IS normal)) THEN (Sepsis IS high_probability)"
+        expected_output = [
+            "IF (gdp_growth_annual_prcnt IS Low) AND (unemployment_rate_value IS High) THEN (PricePrediction IS PricePrediction)",
+            "IF (trade_balance_value IS Low) OR (foreign_direct_investment_value IS Low) THEN (PricePrediction IS PricePrediction)",
+            "IF (spy_close IS High) OR (volume IS Low) THEN (PricePrediction IS PricePrediction)",
+            "IF (gld_close IS Low) AND (macd IS Negative) THEN (PricePrediction IS PricePrediction)",
+            "IF (gld_close IS High) AND (foreign_direct_investment_value IS High) THEN (PricePrediction IS PricePrediction)",
+            "IF (spy_close IS Low) AND (volume IS High) THEN (PricePrediction IS PricePrediction)",
+            "IF (inflation_rate_value IS Medium) THEN (PricePrediction IS PricePrediction)",
+            "IF (gdp_growth_annual_prcnt IS High) OR (NOT (unemployment_rate_value IS Low)) THEN (PricePrediction IS PricePrediction)",
+            "IF (macd IS Positive) OR (rsi IS Oversold) THEN (PricePrediction IS PricePrediction)",
+            "IF (volume IS High) AND (spy_close IS High) THEN (PricePrediction IS PricePrediction)",
+            "IF (PaO2 IS low) AND (Trombocytes IS high) AND (Creatinine IS high) AND (BaseExcess IS normal) THEN (Sepsis IS low_probability)",
+            "IF (PaO2 IS high) AND (Trombocytes IS low) AND (Creatinine IS low) AND (NOT (BaseExcess IS normal)) THEN (Sepsis IS high_probability)"
+        ]
 
-#         ]
+        for rule, expected in zip(rules, expected_output):
+            with self.subTest(rule=rule):
+                self.assertEqual(format_rule(rule), expected)
 
-
-#         formatted_rules = RuleProcessor.format_rules(rules)
-#         for actual, expected in zip(formatted_rules, expected_output):
-#             self.assertEqual(actual, expected, f"Expected '{expected}', got '{actual}'")
-
-import unittest
-from rule_processor import strip_parentheses, find_clauses, reintroduce_parentheses, handle_not_conditions, format_rule
+if __name__ == '__main__':
+    unittest.main()
 
 class TestRuleProcessor(unittest.TestCase):
     
@@ -73,6 +70,23 @@ class TestRuleProcessor(unittest.TestCase):
         rule = "IF ((PaO2 IS high) AND ((Trombocytes IS low) AND ((Creatinine IS low) AND ( NOT (BaseExcess IS normal))))) THEN (Sepsis IS high_probability)"
         expected = "IF (PaO2 IS high) AND (Trombocytes IS low) AND (Creatinine IS low) AND (NOT (BaseExcess IS normal)) THEN (Sepsis IS high_probability)"
         self.assertEqual(format_rule(rule), expected)
+    
+    def test_finalize_not_conditions(self):
+        rules_with_expected = [
+            ("IF (PaO2 IS high) AND  (NOT (Trombocytes IS low)) THEN Sepsis IS high_probability", 
+             "IF (PaO2 IS high) AND (NOT (Trombocytes IS low)) THEN Sepsis IS high_probability"),
+            ("IF (PaO2 IS high) AND (NOT  (Trombocytes IS low)) THEN Sepsis IS high_probability",
+             "IF (PaO2 IS high) AND (NOT (Trombocytes IS low)) THEN Sepsis IS high_probability"),
+            ("IF (PaO2 IS high) AND NOT (Trombocytes IS low) THEN Sepsis IS high_probability",
+             "IF (PaO2 IS high) AND (NOT (Trombocytes IS low)) THEN Sepsis IS high_probability"),
+            ("IF (PaO2 IS high) OR  (NOT (Trombocytes IS low)) THEN Sepsis IS high_probability",
+             "IF (PaO2 IS high) OR (NOT (Trombocytes IS low)) THEN Sepsis IS high_probability")
+        ]
+
+        for rule, expected in rules_with_expected:
+            with self.subTest(rule=rule):
+                self.assertEqual(finalize_not_conditions(rule), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
